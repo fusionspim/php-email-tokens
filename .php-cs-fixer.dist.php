55f13d02cdcf043cf44b6d1fbf8aae1ad04cc974
Andy Stafford <andy@fusionspim.com>
2021-11-24 09:47:51 +0000

    Fix

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,4 +1,4 @@
 <?php
 return FusionsPim\PhpCsFixer\Factory::fromDefaults([
-   'ordered_class_elements' => true, // Order methods too (our default is to leave methods alone)
+    'ordered_class_elements' => true, // Order methods too (our default is to leave methods alone)
 ]);
