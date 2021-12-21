d8725deaf5929efd9ad4c37a5d3c1ccc039c726a
Jamie York <ziadoz@gmail.com>
2021-12-21 13:43:47 +0000

    Upgrade to PHP 8.0 (#76)
    * Remove PHP 7.4 from CI builds.
    
    * Update Docker to PHP 8.
    
    * Update .gitattributes.
    
    * Update packages to PHP 8.
    
    * Install Rector.
    
    * Add Rector alias.
    
    * Configure Rector.
    
    * Upgrade code to PHP 8.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,4 +1,9 @@
 <?php
-return FusionsPim\PhpCsFixer\Factory::fromDefaults([
+$config = FusionsPim\PhpCsFixer\Factory::fromDefaults([
     'ordered_class_elements' => true, // Order methods too (our default is to leave methods alone)
 ]);
+
+return $config->setFinder(
+    $config->getFinder()
+        ->notName('rector.php')
+);
