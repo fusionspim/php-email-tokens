e33e56d69b94cdb24e51edb8db0c39f18b82f444
Jamie <ziadoz@gmail.com>
2022-02-15 12:10:25 +0000

    CS Fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,9 +1,4 @@
 <?php
-$config = FusionsPim\PhpCsFixer\Factory::fromDefaults([
+return FusionsPim\PhpCsFixer\Factory::fromDefaults([
     'ordered_class_elements' => true, // Order methods too (our default is to leave methods alone)
 ]);
-
-return $config->setFinder(
-    $config->getFinder()
-        ->notName('rector.php')
-);
